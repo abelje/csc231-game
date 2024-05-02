1fd88a80f11061e1166eb432ffa83ed0162ebac9
abelje <161378812+abelje@users.noreply.github.com>
2024-05-02 13:29:39 -0500

    removed commented/useless code

--- content/items/spiked_club.cpp
+++ content/items/spiked_club.cpp
@@ -12,14 +12,7 @@ SpikedClub::SpikedClub(int damage)
 
 void SpikedClub::use(Engine& engine, Entity& attacker, Entity& defender) {
     auto swing = engine.events.create_event<Swing>(sprite, attacker.get_direction());
-//    std::shared_ptr<Event> int_hit = engine.events.create_event<Hit>(defender, damage);
-//    std::shared_ptr<Event> self_hit = engine.events.create_event<Hit>(attacker, damage / 2);
-//    std::shared_ptr<Event> sound = engine.events.create_event<Sound>("hit");
-//    self_hit->add_next(sound);
-//    int_hit->add_next(self_hit);
     swing->add_next(Hit{defender, damage});
     swing->add_next(Hit{attacker, damage / 2});
     swing->add_next(Sound{"hit"});
-//    engine.events.create_event<Hit>(attacker, damage / 2);
-
 }
